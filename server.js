1ee963c316788d20cb8ed80a237f93455658e924
Tayo Akosile <akosilety9@gmail.com>
2021-05-28 14:22:54 +0100

    About to make my app realtime

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-//external route
+//external routes
 import express from 'express'
-
+// for socket io
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+// for socket io
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
@@ -10,29 +13,46 @@ import route from './routes/route.js'
 import getMessageroute from './routes/groupMessage.js'
 
 const port = process.env.PORT || 5000
+
 const app = express()
-
-
-app.use(cors())
 app.use(express.json())
 dotenv.config()
+app.use(cors())
 
+const httpServer = createServer()
+const io = new Server(httpServer, {
+ cors: {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['my-custom-header'],
+  credentials: true,
+ },
+})
+
+io.on('connection', socket => {
+ console.log('connection made success')
+ socket.on('message', payload => {
+  console.log('Message recieved', payload)
+  io.emit('message', payload)
+ })
+ // ...
+})
 
 app.use('/', route)
 app.use('/', getMessageroute)
 
 // Db Config
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify :false
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
+ useFindAndModify: false,
 })
 const db = mongoose.connection
-
 db.on('open', () => {
-  console.log('Database connected successfully');
+ console.log('Database connected successfully')
 })
+// Db Config
 
-app.listen(port, () => {
-  console.log(`Port listening on port :${port}`)
+httpServer.listen(port, () => {
+ console.log(`Port listening on port :${port}`)
 })
