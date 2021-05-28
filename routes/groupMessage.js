6b576e2a3178b124b847209d5438e5b92bf121f5
Tayo Akosile <akosilety9@gmail.com>
2021-05-28 21:14:20 +0100

    Update:About to start Socket.io to make my app realtime

--- routes/groupMessage.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { messagesForGroupChat,postGroupMessage } from '../controllers/controller.js'
-const router = express.Router()
-
-router.get('/:roomId', messagesForGroupChat)
-
-router.post('/:roomId',postGroupMessage)
-
-export default router
--- routes/route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createNewGroup, allGroupChat } from '../controllers/controller.js'
-const router = express.Router()
-
-router.get('/', allGroupChat)
-
-router.post('/', createNewGroup)
-
-export default router
