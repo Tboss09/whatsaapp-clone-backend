377b3ad3e89227f9c09a4541ad934cf2b55b9c99
Tayo Akosile <akosilety9@gmail.com>
2021-05-30 20:16:01 +0100

    update:Removed orgin to use all

--- sockets/socket.js
+++ sockets/socket.js
@@ -6,7 +6,7 @@ export const httpServer = createServer()
 
 export const io = new Server(httpServer, {
  cors: {
-  origin: 'https://whatsaap-clone-683bb.web.app/',
+  origin: '*',
   methods: ['GET', 'POST'],
   allowedHeaders: ['my-custom-header'],
   credentials: true,
