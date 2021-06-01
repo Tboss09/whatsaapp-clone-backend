e60e88370583c8027104b28287302dd2349daa79
Tayo Akosile <akosilety9@gmail.com>
2021-06-01 19:25:02 +0100

    Added more functionality to the code,fetched first message user sent

--- database/Schema.js
+++ database/Schema.js
@@ -4,13 +4,13 @@ const userSchema = mongoose.Schema({
  name: String,
  message: String,
  date: { type: String, default: Date },
-})
+},{timestamps:true})
 
 const GroupChatSchema = mongoose.Schema({
  name: String,
  user: [userSchema],
  date: { type: String, default: Date },
-})
+},{timestamps:true})
 
 const WhatsappDatabase = mongoose.model('GroupChat', GroupChatSchema)
 
